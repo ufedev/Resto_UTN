30e102806a4a2484532c1f7c5f8397ac321e4d07
Federico Malfasi <federico.malfasi@gmail.com>
2025-06-23 21:36:55 -0300

    Restov3

--- index.mjs
+++ index.mjs
@@ -1,1 +1,46 @@
-sdkjflsaf
+import { input } from "./utils.mjs"
+
+
+while (true) {
+    console.clear()
+    console.log(`
+    Seleccione una opción:
+        1- Iniciar Sesión
+        2- Registrarse
+        3- Salir
+
+    `)
+    const opcion = await input(":")
+
+    switch (opcion) {
+        case "1":
+            console.clear()
+            console.log("Opcion1")
+            await input("")
+            break
+        case "2":
+            console.clear()
+            console.log("Opcion2")
+            await input("")
+            break
+        case "3":
+            console.clear()
+            console.log("Ha salido del programa")
+            process.exit(0)
+        default:
+            console.log("La opción seleccionada no existe")
+            await input("")
+            break
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
--- utils.mjs
+++ utils.mjs
@@ -1,4 +1,10 @@
 import readline from 'readline'
+
+/**
+ * 
+ * @param {*} texto 
+ * @returns string
+ */
 export function input (texto) {
     const rl = readline.createInterface({
         input: process.stdin,
